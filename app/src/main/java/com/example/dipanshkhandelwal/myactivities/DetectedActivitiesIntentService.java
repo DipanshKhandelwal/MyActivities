1ebb8a405ce663d26e10a80754592e6eb467a4be
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-19 17:52:39 +0530

    Overrided onHandleIntent method

---
+++ app/src/main/java/com/example/dipanshkhandelwal/myactivities/DetectedActivitiesIntentService.java
@@ -0,0 +1,38 @@
+package com.example.dipanshkhandelwal.myactivities;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DIPANSH KHANDELWAL on 19-07-2017.
+ */
+
+public class DetectedActivitiesIntentService extends IntentService{
+
+    protected static final String TAG = "detection_is";
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = new ActivityRecognitionResult.extractResult(intent);
+        Intent localIntent = new Intent(Constants.BROADCAST_ACTION);
+
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
+
+        Log.i(TAG, "activities detected");
+
+        // Broadcast the list of detected activities.
+        localIntent.putExtra(Constants.ACTIVITY_EXTRA, detectedActivities);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
+       }
+}
