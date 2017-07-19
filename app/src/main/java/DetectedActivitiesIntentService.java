b399df3758f020af98afee53bf93c209e2e91ad7
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-19 17:31:50 +0530

    extended the java class with IntentService

--- app/src/main/java/DetectedActivitiesIntentService.java
+++ app/src/main/java/DetectedActivitiesIntentService.java
@@ -1,6 +1,20 @@
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by DIPANSH KHANDELWAL on 19-07-2017.
  */
 
-public class DetectedActivitiesIntentService {
+public class DetectedActivitiesIntentService extends IntentService{
+
+    protected static final String TAG = "detection_is";
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
 }
