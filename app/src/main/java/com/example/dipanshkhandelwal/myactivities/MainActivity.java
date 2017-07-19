3a39295a2a4c69fdf09de380bbddaebb69303f00
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-19 18:03:53 +0530

    Added local variables

--- app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener{
 
+    protected  GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+    private Button requestUpdates;
+    private Button removeUpdates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
