61021b2b2030e893ea4c02bf0348a9aebee09634
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-19 18:00:44 +0530

    implemented important methods

--- app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.dipanshkhandelwal.myactivities;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener{
@@ -11,5 +14,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
