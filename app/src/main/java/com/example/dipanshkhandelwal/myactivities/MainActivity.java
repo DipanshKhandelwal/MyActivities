ed65f80a904577389a7a4588906e24f820f4e58c
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-19 19:34:53 +0530

    Added method requestActivityUpdatesButtonHandler

--- app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/myactivities/MainActivity.java
@@ -11,8 +11,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -47,6 +49,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addOnConnectionFailedListener(this)
                 .addApi(ActivityRecognition.API)
                 .build();
+    }
+
+    public void requestActivityUpdatesButtonHandler(View view) {
+        if (!mGoogleApiClient.isConnected()) {
+            Toast.makeText(this, getString(R.string.not_connected),
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(
+                mGoogleApiClient,
+                Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
+                getActivityDetectionPendingIntent()
+        ).setResultCallback(this);
+
+        requestUpdates.setEnabled(true);
+        removeUpdates.setEnabled(false);
     }
 
     @Override
